4af7c68034431fabd24002e2552fe4ac39436cb1
Th3R3alDuk3 <60510941+Th3R3alDuk3@users.noreply.github.com>
2021-01-21 18:12:16 +0100

    add webview template

--- download.py
+++ download.py
@@ -1,12 +1,31 @@
-#!/usr/bin/env python3
+#!python3
 
 import re
-import tqdm
-import pathlib
-import argparse
 import requests
 
-from moviepy.editor import VideoFileClip
+from tqdm import tqdm
+from pathlib import Path
+from argparse import ArgumentParser
+
+
+class WebView:
+
+    __TEMPLATE_FILE = Path("template.html")
+
+    def __init__(self, title: str, video_file_webcams: Path, video_file_deskshare: Path):
+
+        self.__html = self.__TEMPLATE_FILE.read_text().format(
+            title,
+            "320", "240",   # 4:3 format
+            video_file_webcams.absolute(),
+            video_file_webcams.suffix[1:],
+            "800", "450",   # 16:9 format
+            video_file_deskshare.absolute(),
+            video_file_deskshare.suffix[1:]
+        )
+
+    def save(self, directory: Path):
+        directory.joinpath("webview.html").write_text(self.__html)
 
 
 class BigBlueButtonDownloader:
@@ -17,14 +36,14 @@ class BigBlueButtonDownloader:
         r"(?P<video_id>[0-9a-f\-]+)"
     )
 
-    def __init__(self, url):
+    def __init__(self, url: str):
 
         self.__match = re.match(self.__URL_PATTERN, url)
 
         if self.__match is None:
-            raise Exception("invalid url")
+            raise Exception("invalid bbb-url")
 
-    def get_metadata(self, timeout: int = 3, verify: bool = False):
+    def get_metadata(self, timeout: int = 3, verify: bool = True):
 
         response = requests.get(
             "{0}/presentation/{1}/metadata.xml".format(
@@ -38,7 +57,7 @@ class BigBlueButtonDownloader:
 
         return response.text
 
-    def get_video_urls(self, video_extension: str = "webm"):
+    def get_video_urls(self, video_file_extension: str = "webm"):
 
         # order is important
         video_name2subpath = {
@@ -47,23 +66,23 @@ class BigBlueButtonDownloader:
         }
 
         for video_name, video_subpath in video_name2subpath.items():
-            yield ("{0}.{1}".format(
+            yield "{0}.{1}".format(
                 video_name,
-                video_extension
+                video_file_extension
             ), "{0}/presentation/{1}{2}.{3}".format(
                 self.__match.group("website"),
                 self.__match.group("video_id"),
                 video_subpath,
-                video_extension
-            ))
+                video_file_extension
+            )
 
-    def download_videos(self, directory: pathlib.Path, video_extensions: list, chunk_size: int = 1024):
+    def download_videos(self, directory: Path, video_file_extensions: list, chunk_size: int = 1024):
 
-        for video_extension in video_extensions:
+        for video_file_extension in video_file_extensions:
 
             try:
 
-                for video_name, video_url in self.get_video_urls(video_extension):
+                for video_name, video_url in self.get_video_urls(video_file_extension):
 
                     response = requests.get(
                         video_url,
@@ -81,7 +100,7 @@ class BigBlueButtonDownloader:
                         video_size = int(response.headers["Content-Length"])
                         total_size = int(video_size / chunk_size)
 
-                        tqdm_stream = tqdm.tqdm(
+                        tqdm_stream = tqdm(
                             response.iter_content(chunk_size),
                             total=total_size, desc=video_name, unit="KB"
                         )
@@ -96,29 +115,19 @@ class BigBlueButtonDownloader:
             except Exception:
                 continue
 
-    @staticmethod
-    def sync_audio(video_file1: pathlib.Path, video_file2: pathlib.Path):
-
-        video1 = VideoFileClip(video_file1)
-        video2 = VideoFileClip(video_file2)
-
-        video2.audio = video1.audio
-        video2.write_videofile(video_file2)
-
 
 def main():
 
-    parser = argparse.ArgumentParser()
+    parser = ArgumentParser()
 
     parser.add_argument("directory")
     parser.add_argument("url")
-    parser.add_argument("-s", "--sync", action="store_true")
 
     args = parser.parse_args()
 
     bbbd = BigBlueButtonDownloader(args.url)
 
-    directory = pathlib.Path(args.directory)
+    directory = Path(args.directory)
     directory.mkdir(exist_ok=True)
 
     # get presentation metadata
@@ -130,9 +139,8 @@ def main():
     video_files = bbbd.download_videos(directory, ["webm", "mp4"])
     video_files = list(video_files)
 
-    # sync deskshare- with webcams-audio
-    if args.sync:
-        bbbd.sync_audio(*video_files)
+    webview = WebView("TEST", *video_files)
+    webview.save(directory)
 
 
 if __name__ == "__main__":
