742525cf0519ca728194ca2de7e1a22c822a0d4e
Th3R3alDuk3 <60510941+Th3R3alDuk3@users.noreply.github.com>
2021-03-05 10:29:31 +0100

    add video time forwarding via keys 1-6

--- src/bigbluebutton/template.js
+++ src/bigbluebutton/template.js
@@ -19,6 +19,29 @@ window.onload = () => {
         video2.currentTime = video1.currentTime;
     };
 
+    video1.onkeypress = (event) => {
+        switch(event.keyCode) {
+            case 49: // key 1
+                video1.currentTime -= 10;
+                break;
+            case 50: // key 2
+                video1.currentTime += 10;
+                break;
+            case 51: // key 3
+                video1.currentTime -= 30;
+                break;
+            case 52: // key 4
+                video1.currentTime += 30;
+                break;
+            case 53: // key 5
+                video1.currentTime -= 60;
+                break;
+            case 54: // key 6
+                video1.currentTime += 60;
+                break;
+        }
+    }
+
     // TODO: audio
 
     /**/
